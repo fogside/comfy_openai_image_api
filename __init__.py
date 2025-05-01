c196ef1ac4b1ac26ff6ff9dfcdd73d218c683984
unicough <unicough.github@gmail.com>
2025-04-30 19:21:45 -0700

    Adding dependency

--- __init__.py
+++ __init__.py
@@ -6,9 +6,13 @@ __all__ = [
     "WEB_DIRECTORY",
 ]
 
+REQUIRED_PACKAGES = {
+    "openai": ">=1.76.2",   
+}
+
 __author__ = """Xin"""
 __email__ = "unicough.github@gmail.com"
-__version__ = "0.0.1"
+__version__ = "0.0.4"
 
 from .src.openai_image_api.nodes import NODE_CLASS_MAPPINGS
 from .src.openai_image_api.nodes import NODE_DISPLAY_NAME_MAPPINGS
